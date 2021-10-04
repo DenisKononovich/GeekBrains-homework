4e5adf9e8c4c6a0fe4b306419fb802ba8079aeb2
Kononovich Denis <den.kononovich@yandex.by>
2021-10-04 21:50:21 +0300

    Create HomeWorkApp.java
    lesson 1

---
+++ HomeWorkApp.java
@@ -0,0 +1,41 @@
+class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    
+    static void checkSumSign() {
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+
+        System.out.println(c > 0 ? "Сумма положительная" : "Сумма отрицательная");
+    }
+
+    static void printColor() {
+        int value = 5;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    static void compareNumbers() {
+        int a = 3;
+        int b = 4;
+
+        System.out.println(a >= b ? "a >= b" : "a < b");
+    }
+}
